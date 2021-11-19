8a8816d2b85c10c26b47627af31b3149e4ae5118
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-11-19 13:38:23 -0500

    done

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,9 @@
 function recursiveSearch(arr, target) {
   // type your code here
+  if (arr[0]===target) {
+    return true;
+  }
+  return arr.length>0 ? recursiveSearch(arr.slice(1),target) : false;
 }
 
 if (require.main === module) {
